8a787ad57fb20dbf82133c4bee153e150c3fe164
FloydWatson <floydbcseit@gmail.com>
2019-11-10 22:17:05 +1300

    added provider to pass data to products grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -9,16 +10,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
-      ),
-      home: ProductOverviewScreen(),
-      routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),}
+    // register a class that you can listen to with child widgets
+    return ChangeNotifierProvider(
+      // builder returns new instance of provider class. all interested child widgets will get this
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+              primarySwatch: Colors.purple,
+              accentColor: Colors.deepOrange,
+              fontFamily: 'Lato'),
+          home: ProductOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
-
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // set up provider listener. change of method to tell it to listen to instance created in main
+    final productsData = Provider.of<ProductsProvider>(context);
+
+    // getting product list [] from provider
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(
+          10.0), // set const to make sure it does not rebuild. saves performance
+      itemCount: products.length,
+      // item builder is what we will see on the screen
+      itemBuilder: (ctx, i) => ProductItem(products[i].id, products[i].title, products[i].imageUrl),
+      // how grid will be structured. cross axis allows us to define number of coloumns, aspect ratio = bit taller then they are wide. 3 / 2
+      // cross axis spacing = gutering between items, main axis spacing = space between rows
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+      ),
+    );
+  }
+}
